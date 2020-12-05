65f00e6282d7e1989192d8f660fb09c1479b866b
Rouven <seifertrouven@gmail.com>
2020-12-05 17:59:17 +0100

    removed unused imports

--- app/src/main/java/com/traincon/modelleisenbahn_controller/LocoConfigActivity.java
+++ app/src/main/java/com/traincon/modelleisenbahn_controller/LocoConfigActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 
 import android.view.MenuItem;
 
