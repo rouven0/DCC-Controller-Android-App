ae0a3821ccb414db26bafe35d7a2ec828ed17ba2
Rouven <seifertrouven@gmail.com>
2020-12-10 15:02:27 +0100

    added dynamic title change in LocoConfigActivity

--- app/src/main/java/com/traincon/modelleisenbahn_controller/LocoConfigActivity.java
+++ app/src/main/java/com/traincon/modelleisenbahn_controller/LocoConfigActivity.java
@@ -2,23 +2,45 @@ package com.traincon.modelleisenbahn_controller;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.MenuItem;
 
 import java.util.Objects;
 
 public class LocoConfigActivity extends AppCompatActivity {
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loco_config);
-        ActionBar actionBar = getSupportActionBar();
+        actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    protected void onResume() {NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().getPrimaryNavigationFragment();
+        final NavController navController = navHostFragment.getNavController();
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                if (destination.getId() == R.id.AddLocoFragment) {
+                    actionBar.setTitle(R.string.label_loco_add);
+                } else {
+                    actionBar.setTitle(R.string.label_activity_loco_config);
+                }
+            }
+        });
+        super.onResume();
     }
 
     @Override
